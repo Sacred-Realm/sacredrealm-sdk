9cb37b4ea32c8d4bd132776a9bccf9d2cc3a5917
havenquan <havenquan@qq.com>
2022-05-11 01:56:34 +0800

    1.0.7

--- src/constant.ts
+++ src/constant.ts
@@ -14,7 +14,7 @@ export function token(env?: string) {
       SB: '0xa844b28aBf7aF74438131A1aF642Ef0CCAa8C166',
       SN: '0xb2761cd8EFD9f436Efa1b205dEB1dC957300Ede5',
       ST: '0x',
-      SR: '0x',
+      SR: '0x7c3323f3d283b46514600cD9AD3F5e821F679b7f',
       BUSD: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
       WBNB: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
     };
@@ -25,14 +25,18 @@ export function contract(env?: string) {
   const environment = env || process.env.NODE_ENV;
   if (environment === 'production') {
     return {
+      Inviting: '0x',
+      STStaking: '0x',
       BondDepository: '0x',
       SRDeposit: '0x',
       PancakeRouter: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
     };
   } else {
     return {
+      Inviting: '0x',
+      STStaking: '0x',
       BondDepository: '0x',
-      SRDeposit: '0x',
+      SRDeposit: '0x2e8d3785b157104Fa0cF8436499367A64bE0caf1',
       PancakeRouter: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     };
   }
