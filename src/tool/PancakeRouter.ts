66b770363708480457111f5cbb1b4ad366d3cb43
havenquan <havenquan@qq.com>
2022-08-08 10:44:19 +0800

    pan

--- src/tool/PancakeRouter.ts
+++ src/tool/PancakeRouter.ts
@@ -2,6 +2,6 @@ import { IPancakeRouter__factory } from 'sealemlab-core/typechain-types'
 import { rpcProvider } from '../utils';
 import { contract } from '../constant';
 
-export function iPancakeRouter() {
+export function pancakeRouter() {
   return IPancakeRouter__factory.connect(contract().PancakeRouter, rpcProvider);
 }
